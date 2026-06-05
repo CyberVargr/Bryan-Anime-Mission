a4c2122c65f9cccb748b4e8810d3b04bf923f406
CyberVargr <gurden.clark@gmail.com>
2026-06-05 23:58:28 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,1 @@
-const CACHE_NAME = "anime-mission-v3";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
+const CACHE_NAME="anime-mission-v4";const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
